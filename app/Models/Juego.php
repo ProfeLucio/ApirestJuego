d12e8d7c2088f8d23affd5bd3a3c915214000d11
Gonzalo Lucio <glucio@dnp.gov.co>
2025-06-03 13:05:17 -0500

    UUID games

--- app/Models/Juego.php
+++ app/Models/Juego.php
@@ -3,6 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Juego;
+use App\Models\User;
+use Illuminate\Support\Str;
+use App\Models\Partida;
 
 class Juego extends Model
 {
